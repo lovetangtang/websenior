e831715c2f434d951e77cef7240e7f113828306b
江 唐 <764053787@qq.com>
2019-10-14 22:06:12 +0800

    WEBAPI

---
+++ JS/第四课AJAX/WebApi/WebApi/Controllers/AjaxApiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class AjaxApiController : ApiController
+    {
+        // GET: api/AjaxApi
+        public string Get(string name)
+        {
+            return "你好"+name+"，今天天气不错";
+        }
+
+        // GET: api/AjaxApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/AjaxApi
+        public string Post(dynamic name)
+        {
+            return "你好" + name.name + "，今天天气不错";
+        }
+
+        // PUT: api/AjaxApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/AjaxApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
